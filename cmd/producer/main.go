b06fc49a1f34e0fa76dfc6fce25f1a6ea8752312
Rafiere <rafapgbr@gmail.com>
2024-01-29 09:47:31 -0300

    feat: add config map for first producer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+)
 
 func main() {
 
 	fmt.Println("Hello, Go!")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "16-apache-kafka-kafka-1:9092",
+	}
+
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return p
 }
