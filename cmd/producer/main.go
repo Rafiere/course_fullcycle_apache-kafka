a318d801dba03c852961a4ff5f3d8a36d690e105
Rafiere <rafapgbr@gmail.com>
2024-01-30 08:39:52 -0300

    feat: add consumer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -8,6 +8,24 @@ import (
 
 func main() {
 
+	deliveryChan := make(chan kafka.Event)
+	producer := NewKafkaProducer()
+
+	Publish("mensagem", "teste2", producer, nil, deliveryChan)
+
+	go DeliveryReport(deliveryChan) //async
+
+	/* Abaixo, estamos pegando o resultado do envio da mensagem. */
+	//e := <-deliveryChan //Enquanto não vier uma mensagem para o delivery channel, o programa ficará pausado. Isso não é uma boa prática pois torna a comunicação síncrona.
+	//msg := e.(*kafka.Message)
+	//
+	//if msg.TopicPartition.Error != nil {
+	//	fmt.Println("Erro ao enviar mensagem")
+	//} else {
+	//	fmt.Println("Mensagem enviada:", msg.TopicPartition)
+	//}
+	//producer.Flush(1000)
+
 	fmt.Println("Hello, Go!")
 }
 
@@ -23,4 +41,38 @@ func NewKafkaProducer() *kafka.Producer {
 	}
 
 	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
+
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/* Todas as vezes que uma nova mensagem chegar, o loop abaixo será executado. */
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar mensagem")
+			} else {
+				fmt.Println("Mensagem enviada:", ev.TopicPartition)
+				//Podemos anotar no banco de dados que a mensagem foi processada
+				//Por exemplo, confirmando que uma transferência bancária ocorreu.
+
+				//Se algum problema aconteceu, podemos tentar realizar o "retry".
+			}
+		}
+	}
 }
